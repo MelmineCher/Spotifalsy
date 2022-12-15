0cac37a86599610a63ec718ba2321dfa36aa1f68
MelmineCher <117828155+MelmineCher@users.noreply.github.com>
2022-12-15 10:03:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php require('../html/header.html')?>
+<?php require ('../curl/curl_albums.php') ?>
+
+
+
+<div class="content">
+    <h1 class="heading"> Bienvenue sur <span>Spotifalsy</span></h1>
+    <button onclick="window.location.href ='allalbums.php'" class="button" >Parcourir les albums</button>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require('../html/footer.html')?>
